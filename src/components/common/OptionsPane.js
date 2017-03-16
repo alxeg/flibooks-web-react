ae8bc09a01e5aeae11a0e2b1b4939d6c17341eee
alxeg <alx.egorov@gmail.com>
2017-03-16 14:33:08 +0300

    Style options pane

--- src/components/common/OptionsPane.js
+++ src/components/common/OptionsPane.js
@@ -3,6 +3,7 @@ import React, { Component, PropTypes } from 'react';
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import Checkbox from 'material-ui/Checkbox';
+import Paper from 'material-ui/Paper';
 
 import { Grid, Cell } from 'react-flexr';
 
@@ -17,21 +18,23 @@ const OptionsPane = ({open, options, onChanged}) => {
             onRequestChange={onChanged}
         >
             <AppBar title="Options" />
-
-            {options.langs &&
-                <Grid>
-                        {Object.keys(options.langs).map( lang => {
-                            return (
-                                <Cell key={lang}>
-                                    <Checkbox
-                                        label={lang}
-                                        defaultChecked={options.langs[lang]}
-                                    />
-                                </Cell>
-                            );
-                        })}
-                </Grid>
-            }
+            <Paper zDepth={2} rounded={false} style={{margin: '10px', padding: '5px', height: 'calc(100% - 75px)' }}>
+                {options.langs &&
+                    <Grid align="center">
+                            {Object.keys(options.langs).map( lang => {
+                                return (
+                                    <Cell key={lang} size="1/3">
+                                        <Checkbox
+                                            style={{padding: '5px'}}
+                                            label={lang}
+                                            defaultChecked={options.langs[lang]}
+                                        />
+                                    </Cell>
+                                );
+                            })}
+                    </Grid>
+                }
+            </Paper>
         </Drawer>
     );
 };
